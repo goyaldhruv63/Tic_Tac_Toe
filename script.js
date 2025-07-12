bb5668e7a0434a7ea208128f72ae0255414e2666
Dhruv Goyal <goyaldhruv63@gmail.com>
2025-07-12 20:34:22 +0530

    Initial commit: Add Tic Tac Toe Game

---
+++ script.js
@@ -0,0 +1,76 @@
+let boxes = document.querySelectorAll(".box");
+let resetButton = document.querySelector(".reset");
+let newGameButton = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+
+let turnO = true;
+const winpatterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+]
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        // Player O
+        if (turnO) {
+            box.innerText = "O"
+            turnO = false;
+        }
+        // Player X
+        else {
+            box.innerText = "X"
+            turnO = true;
+        }
+        box.disabled = true;
+
+        checkWinner()
+
+    })
+})
+
+const disableBoxes = () => {
+    for (let box of boxes) {
+        box.disabled = true;
+    }
+}
+const enableBoxes = () => {
+    for (let box of boxes) {
+        box.disabled = false;
+        box.innerText = "";
+    }
+}
+
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations! ${winner} is the winner!`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+}
+
+const resetGame = () => {
+    turnO = true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+}
+
+const checkWinner = () => {
+    for (let pattern of winpatterns) {
+        let pos1 = boxes[pattern[0]].innerText;
+        let pos2 = boxes[pattern[1]].innerText;
+        let pos3 = boxes[pattern[2]].innerText;
+
+        if (pos1 === pos2 && pos2 === pos3 && pos1 !== "") {
+            showWinner(pos1);
+        }
+    }
+}
+
+newGameButton.addEventListener("click", resetGame)
+resetButton.addEventListener("click", resetGame)
